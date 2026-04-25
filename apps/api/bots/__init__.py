dcb19a25809e3addd2e29af8be49bc4c4807dc32
neubaezchet <davidbaezaospino@gmail.com>
2026-04-25 08:51:55 -0500

    Update __init__.py

--- apps/api/bots/__init__.py
+++ apps/api/bots/__init__.py
@@ -1,4 +1,15 @@
-from bots.base import DatosIncapacidad, ResultadoRadicacion
+from bots.base import (
+    CredencialesEmpleador,
+    DatosIncapacidad,
+    ResultadoRadicacion,
+    TIPOS_DOCUMENTO_VALIDOS,
+)
 from bots.sura import radicar_en_sura
 
-__all__ = ["DatosIncapacidad", "ResultadoRadicacion", "radicar_en_sura"]
+__all__ = [
+    "CredencialesEmpleador",
+    "DatosIncapacidad",
+    "ResultadoRadicacion",
+    "TIPOS_DOCUMENTO_VALIDOS",
+    "radicar_en_sura",
+]
